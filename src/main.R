1658b8a368f22d87acd80458023fc7f021155569
Brian Pondi <brianpondi.geogeek@gmail.com>
2022-05-15 21:24:13 +0200

    Refactor gdalcubes and use stac on AWS

--- src/main.R
+++ src/main.R
@@ -16,7 +16,7 @@ gdalcubes_options(parallel = 8)
 #con = connect(host = "https://openeo.cloud")
 
 # Gdalcube global variable
-stac_data <- NULL
+stac_items <- NULL
 data_cube <- NULL
 
 #* @apiTitle EO Lightweight Platform
@@ -24,44 +24,37 @@ data_cube <- NULL
 
 
 #* Discover available satellite imagery in your region of interest
-#* @param bbox  e.g. 45.0,-20.1,47.0, -19.8
-#* @param date_time e.g. 2020-01-01/2020-06-31
-#* @param collection_type e.g. s2_l2a or ls8_sr
+#* @param min_x 7.1
+#* @param min_y 51.8
+#* @param max_x 7.2
+#* @param max_y 52.8
+#* @param datetime_range 2021-01-01/2021-06-31
+#* @param collection_type sentinel-s2-l2a-cogs
 #* @get /v1/discover-data
-function(bbox = "", date_time= "", collection_type = "") {
-  s_obj <- stac("https://explorer.digitalearth.africa/stac")
-  it_obj <- s_obj %>% stac_search(collections = "ls8_sr",
-                      datetime = "2021-01-01/2021-03-31",
-                      bbox = c(45.0, -20.1, 47.0, -19.8)) %>% get_request()            
+function(min_x = "", min_y = "", max_x = "", max_y = "", datetime_range= "", collection_type = "") {
+  #Convert bbox values to numeric
+  min_x <- as.numeric(min_x)
+  min_y <- as.numeric(min_y)
+  max_x <- as.numeric(max_x)
+  max_y <- as.numeric(max_y)
+  #Connect to STAC API and get sentinel data
+  stac_object = stac("https://earth-search.aws.element84.com/v0")
+  items = stac_object %>%
+    stac_search(collections = "sentinel-s2-l2a-cogs",
+              bbox = c(7.1, 51.8, 7.2, 52.8), 
+              datetime = "2021-01-01/2021-03-31") %>%
+    post_request() %>% items_fetch() 
+  # Assign to global variable for stac_items
+  stac_items <<- items          
 }
 
 #* Create gdalcubes for your region of interest
-#* @param bbox  e.g. 45.0,-20.1,47.0, -19.8
-#* @param date_time e.g. 2020-01-01/2020-06-31
-#* @param collection_type e.g. s2_l2a or ls8_sr
 #* @get /v1/create-gdalcubes
 #* @serializer unboxedJSON
-function(bbox = "", date_time= "", collection_type = "") {
-  # get data from stac
-  s_obj <- s_obj <- stac("https://explorer.digitalearth.africa/stac")
-  it_obj <- s_obj %>% stac_search(collections = "ls8_sr",
-                      datetime = "2021-01-01/2021-03-31",
-                      bbox = c(45.0, -20.1, 47.0, -19.8)) %>% get_request()
-  # modify global variable to contain stac data
-  #stac_data <<- it_obj 
-  #--------------------------------------------------------------------------------#
-  # stac collection url
-  stac_url <- "https://explorer.digitalearth.africa/stac/search?collection=ls8_sr&time=2022-01-01/2022-03-31&bbox=[45.0,-20.1,47.0,-19.8]"
-  
-  # get response from stac collection
-  req <- request(stac_url)
-  resp <- req_perform(req)
-  resp_json <- resp_body_json(resp)
-  
-  
-  # create image collection from stac api response
+function() {
+  # create image collection from stac items features
   img.col <- stac_image_collection(
-    resp_json$features
+    stac_items$features
   )
   
   # Define cube view with onthly aggregation, 100 Metres dimension
@@ -70,7 +63,7 @@ function(bbox = "", date_time= "", collection_type = "") {
   # Gdalcubes creation
   cube.overview = raster_cube(img.col, v.overview)
   # Assign to a global variable
-  stac_data <<- cube.overview
+  data_cube <<- cube.overview
   
   # Response JSON to user
   msg <- list(status = "SUCCESS", code = "200",message ="gdalcubes object created successfully")
@@ -79,15 +72,16 @@ function(bbox = "", date_time= "", collection_type = "") {
 #* Get list of OpenEO processes
 #* @get /v1/processes/open-eo
 function() {
-  #TO DO
-  processes_list <- list("filter_bands", "filter_spatial", "ndvi", "mask", "save_result")
+  # TO DO, Implement some openeo processes that work on a raster-cube
+  # https://processes.openeo.org/#filter_bbox
+  processes_list <- list("filter_bands", "filter_bbox", "resample_spatial", "save_result")
 }
 
 #* Run OpenEO process
 #* @post /v1/run/processes/open-eo
 function() {
   #TO DO
-  stac_data
+  stac_items
 }
 
 #* Validate a user-defined process
@@ -103,6 +97,7 @@ function(user_defined_process) {
 #* @param user_defined_process User-defined function
 #* @post /v1/run/user-defined-process
 function(user_defined_process) {
+  #focus on apply pixel for a start
     func_parse <- parse(text = user_defined_process)
     user_function <- eval(func_parse)
 }

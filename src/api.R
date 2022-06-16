0d06e10774f717103c64c36319083256a79b754d
Brian Pondi <brianpondi.geogeek@gmail.com>
2022-06-16 17:09:53 +0200

    Code formatting

--- src/api.R
+++ src/api.R
@@ -17,7 +17,7 @@ library(botor)
 gdalcubes_options(parallel = 16)
 
 #Surpress warnings
-options(warn=-1)
+options(warn = -1)
 
 
 #set workdir
@@ -38,8 +38,20 @@ data_cube <- NULL
 processes_list <- function() {
   # TO DO, Implement some openeo processes that work on a raster-cube
   # https://processes.openeo.org
-  processes_list <- list("filter_bands", "filter_bbox","filter_spatial", "filter_temporal","load_collection",
-                         "merge_cubes","reduce_dimension","rename_dimension","rename_labels","run_udf", "save_result")
+  processes_list <-
+    list(
+      "filter_bands",
+      "filter_bbox",
+      "filter_spatial",
+      "filter_temporal",
+      "load_collection",
+      "merge_cubes",
+      "reduce_dimension",
+      "rename_dimension",
+      "rename_labels",
+      "run_udf",
+      "save_result"
+    )
 }
 
 #* Discover available satellite imagery in your region of interest
@@ -50,22 +62,30 @@ processes_list <- function() {
 #* @param time_range 2021-01-01/2021-06-30
 #* @param collection  sentinel-s2-l2a-cogs
 #* @get /v1/stac/discover_data
-discover_data <- function(xmin = "7.1", ymin = "51.8", xmax = "7.2", ymax = "52.8", time_range = "2021-01-01/2021-06-30", collection = "sentinel-s2-l2a-cogs") {
-  #Convert bbox values to numeric
-  xmin <- as.numeric(xmin)
-  ymin <- as.numeric(ymin)
-  xmax <- as.numeric(xmax)
-  ymax <- as.numeric(ymax)
-  #Connect to STAC API and get sentinel data
-  stac_object = stac("https://earth-search.aws.element84.com/v0")
-  items = stac_object %>%
-    stac_search(collections = collection,
-              bbox = c(xmin,ymin,xmax,ymax),
-              datetime = time_range) %>%
-    post_request() %>% items_fetch()
-  # Assign to global variable for stac_items
-  stac_items <<- items
-}
+discover_data <-
+  function(xmin = "7.1",
+           ymin = "51.8",
+           xmax = "7.2",
+           ymax = "52.8",
+           time_range = "2021-01-01/2021-06-30",
+           collection = "sentinel-s2-l2a-cogs") {
+    #Convert bbox values to numeric
+    xmin <- as.numeric(xmin)
+    ymin <- as.numeric(ymin)
+    xmax <- as.numeric(xmax)
+    ymax <- as.numeric(ymax)
+    #Connect to STAC API and get sentinel data
+    stac_object = stac("https://earth-search.aws.element84.com/v0")
+    items = stac_object %>%
+      stac_search(
+        collections = collection,
+        bbox = c(xmin, ymin, xmax, ymax),
+        datetime = time_range
+      ) %>%
+      post_request() %>% items_fetch()
+    # Assign to global variable for stac_items
+    stac_items <<- items
+  }
 
 #* Loads a collection and returns a processable data cube(gdalcube).
 #* @param id collection
@@ -76,9 +96,13 @@ discover_data <- function(xmin = "7.1", ymin = "51.8", xmax = "7.2", ymax = "52.
 #* @param temporal_resolution P1M = Monthly, P3M = Quarterly
 #* @post /v1/processes/open-eo/load_collection
 #* @serializer unboxedJSON
-load_collection <- function(id ="sentinel-s2-l2a-cogs", bbox ="7.1,51.8,7.2,52.8",
-                            temporal_extent ="2021-01-01/2021-06-30", bands = "B04,B08",
-                            spatial_resolution="250",temporal_resolution = "P1M"){
+load_collection <-
+  function(id = "sentinel-s2-l2a-cogs",
+           bbox = "7.1,51.8,7.2,52.8",
+           temporal_extent = "2021-01-01/2021-06-30",
+           bands = "B04,B08",
+           spatial_resolution = "250",
+           temporal_resolution = "P1M") {
     ##bbox to numeric
     bbox.split <- str_split(bbox, ",")
     bbox.unlist <- unlist(bbox.split)
@@ -89,22 +113,28 @@ load_collection <- function(id ="sentinel-s2-l2a-cogs", bbox ="7.1,51.8,7.2,52.8
     #Connect to STAC API and get sentinel data
     stac_object = stac("https://earth-search.aws.element84.com/v0")
     items = stac_object %>%
-      stac_search(collections = id,
-                  bbox = c(xmin,ymin,xmax,ymax),
-                  datetime = temporal_extent) %>%
+      stac_search(
+        collections = id,
+        bbox = c(xmin, ymin, xmax, ymax),
+        datetime = temporal_extent
+      ) %>%
       post_request() %>% items_fetch()
     # create image collection from stac items features
-    img.col <- stac_image_collection(
-       items$features
-    )
+    img.col <- stac_image_collection(items$features)
     # Define cube view with monthly aggregation, 250 Metres dimension
     spatial_resolution <- as.numeric(spatial_resolution)
-    v.overview = cube_view(srs="EPSG:3857", extent=img.col,
-                           dx=spatial_resolution, dy=spatial_resolution,
-                           dt = temporal_resolution, resampling="average", aggregation="median")
+    v.overview = cube_view(
+      srs = "EPSG:3857",
+      extent = img.col,
+      dx = spatial_resolution,
+      dy = spatial_resolution,
+      dt = temporal_resolution,
+      resampling = "average",
+      aggregation = "median"
+    )
     # gdalcubes creation
     cube = raster_cube(img.col, v.overview)
-    if(! is.null(bands)){
+    if (!is.null(bands)) {
       #split user input
       bands.split <- str_split(bands, ",")
       bands.unlist <- unlist(bands.split)
@@ -112,35 +142,42 @@ load_collection <- function(id ="sentinel-s2-l2a-cogs", bbox ="7.1,51.8,7.2,52.8
       cube = select_bands(cube, bands.unlist)
     }
 
-  # Assign to a global variable
-   data_cube <<- cube
+    # Assign to a global variable
+    data_cube <<- cube
 
-  # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="gdalcubes object created successfully")
-}
+    # Response msg to user
+    msg <-
+      list(status = "SUCCESS",
+           code = "200",
+           message = "gdalcubes object created successfully")
+  }
 
 
 #* Select bands from gdalcube
 #* @param bands B04,B08
 #* @post /v1/processes/open-eo/filter_bands
-filter_bands <- function( data= data_cube, bands="B04,B08"){
-    if(is.null(bands)){
-      stop("The bands values should not be empty")
-    }
-    #split user input
-    bands.split <- str_split(bands, ",")
-    bands.unlist <- unlist(bands.split)
-    cube = select_bands(data, bands.unlist)
+filter_bands <- function(data = data_cube, bands = "B04,B08") {
+  if (is.null(bands)) {
+    stop("The bands values should not be empty")
+  }
+  #split user input
+  bands.split <- str_split(bands, ",")
+  bands.unlist <- unlist(bands.split)
+  cube = select_bands(data, bands.unlist)
 
-    data_cube <<- cube
+  data_cube <<- cube
   # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="gdalcubes bands filtered successfully")
+  msg <-
+    list(status = "SUCCESS",
+         code = "200",
+         message = "gdalcubes bands filtered successfully")
 }
 
 #* Limits the data cube to the specified bounding box.
 #* @param bbox 7.1,51.8,7.2,52.8
 #* @post /v1/processes/open-eo/filter_bbox
-filter_bbox <- function(data = data_cube, bbox = "7.1,51.8,7.2,52.8"){
+filter_bbox <-
+  function(data = data_cube, bbox = "7.1,51.8,7.2,52.8") {
     ##bbox to numeric
     bbox.split <- str_split(bbox, ",")
     bbox.unlist <- unlist(bbox.split)
@@ -150,33 +187,35 @@ filter_bbox <- function(data = data_cube, bbox = "7.1,51.8,7.2,52.8"){
     ymax <- as.numeric(bbox.unlist[4])
 
     ##create sf points
-    pt1 <- st_point(c(xmin,ymin))
-    pt2 <- st_point(c(xmin,ymax))
-    pt3 <- st_point(c(xmax,ymax))
-    pt4 <- st_point(c(xmax,ymin))
-    pt5 <- st_point(c(xmin,ymin))
+    pt1 <- st_point(c(xmin, ymin))
+    pt2 <- st_point(c(xmin, ymax))
+    pt3 <- st_point(c(xmax, ymax))
+    pt4 <- st_point(c(xmax, ymin))
+    pt5 <- st_point(c(xmin, ymin))
 
     ##create polygon
     pts <- list(rbind(pt1, pt2, pt3, pt4, pt5))
     poly <- st_polygon(pts)
-    poly <-st_sfc(poly,crs = 3857)
+    poly <- st_sfc(poly, crs = 3857)
 
     #filter data cube
-    cube <- filter_geom(data,poly)
+    cube <- filter_geom(data, poly)
 
-  # rewrite filtered cubes to the global variable
-  data_cube <<- cube
-  # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="gdalcubes filtered by bounding box successfully")
+    # rewrite filtered cubes to the global variable
+    data_cube <<- cube
+    # Response msg to user
+    msg <-
+      list(status = "SUCCESS",
+           code = "200",
+           message = "gdalcubes filtered by bounding box successfully")
 
-}
+  }
 
 
 #* Spatial filter using geometries.
 #* @param geometries
 #* @post /v1/processes/open-eo/filter_spatial
-filter_spatial <- function(data = data_cube,geometries = ""){
-
+filter_spatial <- function(data = data_cube, geometries = "") {
 
 }
 
@@ -185,51 +224,65 @@ filter_spatial <- function(data = data_cube,geometries = ""){
 #* Temporal filter based on temporal intervals.
 #* @param extent 2022-01-01,2022-03-30
 #* @post /v1/processes/open-eo/filter_temporal
-filter_temporal <- function(data = data_cube,extent = "2021-01-01,2021-03-30"){
+filter_temporal <-
+  function(data = data_cube, extent = "2021-01-01,2021-03-30") {
     extent.split <- str_split(extent, ",")
     extent.unlist <- unlist(extent.split)
     cube <- select_time(data, c(extent.unlist[1], extent.unlist[2]))
 
-  # overwrite global variable
-  data_cube <<- data_cube.time
-  # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="gdalcubes filtered by time interval successfully")
+    # overwrite global variable
+    data_cube <<- data_cube.time
+    # Response msg to user
+    msg <-
+      list(status = "SUCCESS",
+           code = "200",
+           message = "gdalcubes filtered by time interval successfully")
 
-}
+  }
 
 #* Renames a dimension in the data cube while preserving all other properties
 #* @param dimension bands
 #* @param target red
 #* @param source B01
 #* @post /v1/processes/open-eo/rename_dimension
-rename_dimension <- function(data=data_cube, source="B01", target="red"){
-
+rename_dimension <-
+  function(data = data_cube,
+           source = "B01",
+           target = "red") {
     cube <- rename_bands(data, source = target)
 
-  #override global
-  data_cube <<- cube
-  # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="Renaming of dimension applied")
-}
+    #override global
+    data_cube <<- cube
+    # Response msg to user
+    msg <-
+      list(status = "SUCCESS",
+           code = "200",
+           message = "Renaming of dimension applied")
+  }
 
 #* Rename dimension labels
 #* @param dimension bands
 #* @param target red,green,blue
 #* @param source B01,B02,B03
 #* @post /v1/processes/open-eo/rename_labels
-rename_labels <- function(data  = data_cube,dimension="bands", target="red,green,blue", source="B01,B02,B03"){
+rename_labels <-
+  function(data  = data_cube,
+           dimension = "bands",
+           target = "red,green,blue",
+           source = "B01,B02,B03") {
 
-
-}
+  }
 
 
 #* Reduce dimensions
 #* @param reducer
 #* @param dimension time or bands
 #* @post /v1/processes/open-eo/reduce_dimension
-reduce_dimension <-function(data = data_cube,reducer="", dimension=""){
-    if(dimension == "time") {
-
+reduce_dimension <-
+  function(data = data_cube,
+           reducer = "",
+           dimension = "") {
+    if (dimension == "time") {
       bands = bands(data)$name
       bandStr = c()
 
@@ -241,7 +294,6 @@ reduce_dimension <-function(data = data_cube,reducer="", dimension=""){
       return(cube)
     }
     else if (dimension == "bands") {
-
       cube = apply_pixel(data, reducer, keep_bands = FALSE)
       return(cube)
     }
@@ -249,33 +301,41 @@ reduce_dimension <-function(data = data_cube,reducer="", dimension=""){
       stop('Kindly select "time" or "bands" as dimension')
     }
 
-  # rewrite cubes to the global variable
-  data_cube <<- cube
-  # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="Dimensions reduced successfully")
+    # rewrite cubes to the global variable
+    data_cube <<- cube
+    # Response msg to user
+    msg <-
+      list(status = "SUCCESS",
+           code = "200",
+           message = "Dimensions reduced successfully")
 
-}
+  }
 
 
 #* Merge two data cubes **Experimental
 #* @param datacube2
 #* @post /v1/processes/open-eo/merge_cubes
-merge_cubes <- function(datacube1 = data_cube , datacube2= ""){
-    #check if they are not datacubes
-    `%!in%` <- Negate(`%in%`)
-    if("cube" %!in% class(datacube1) && "cube" %!in% class(datacube2)) {
-      stop('Provided cubes are not of class "cube"')
-    }
-    #check if the datacubes have equal dimesions
-    compare = compare.list(dimensions(datacube1), dimensions(datacube2))
-    if(FALSE %in% compare) {
-      stop("Dimensions of the datacubes provided are not equal")
-    }
-    cube = join_bands(c(cube1, cube2))
+merge_cubes <- function(datacube1 = data_cube ,
+                        datacube2 = "") {
+  #check if they are not datacubes
+  `%!in%` <- Negate(`%in%`)
+  if ("cube" %!in% class(datacube1) &&
+      "cube" %!in% class(datacube2)) {
+    stop('Provided cubes are not of class "cube"')
+  }
+  #check if the datacubes have equal dimesions
+  compare = compare.list(dimensions(datacube1), dimensions(datacube2))
+  if (FALSE %in% compare) {
+    stop("Dimensions of the datacubes provided are not equal")
+  }
+  cube = join_bands(c(cube1, cube2))
 
   data_cube <<- cube
   # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="Process applied successfully")
+  msg <-
+    list(status = "SUCCESS",
+         code = "200",
+         message = "Process applied successfully")
 
 }
 
@@ -283,37 +343,55 @@ merge_cubes <- function(datacube1 = data_cube , datacube2= ""){
 #* Run a user defined process on gdalcubes
 #* @param udf User-defined function
 #* @post /v1/processes/open-eo/run_udf
-run_udf <- function(data= data_cube, udf="", runtime=NULL){
-    #convert parsed string function to class function
-    func_parse <- parse(text = udf)
-    user_function <- eval(func_parse)
-    #TO DO, how to identify a reduce or apply process
-    if(process =="reduce"){
-      results <- reduce_time(data,FUN = user_function)
-    }else if (process == "apply"){
-      results <- apply_pixel(data,FUN = user_function)
-    }
+run_udf <- function(data = data_cube,
+                    udf = "",
+                    runtime = NULL) {
+  #convert parsed string function to class function
+  func_parse <- parse(text = udf)
+  user_function <- eval(func_parse)
+  #TO DO, how to identify a reduce or apply process
+  if (process == "reduce") {
+    results <- reduce_time(data, FUN = user_function)
+  } else if (process == "apply") {
+    results <- apply_pixel(data, FUN = user_function)
+  }
 
   data_cube <<- results
   # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="UDF  applied successfully")
+  msg <-
+    list(status = "SUCCESS",
+         code = "200",
+         message = "UDF  applied successfully")
 
 }
 
 #* Save processed data
 #* @param format TIFF or NetCDF
 #* @post /v1/processes/open-eo/save_result
-save_result <- function(data,format="TIFF"){
-    if(is.null(format) || tolower(format) =="tiff" || format =="") {
-      write_tif(data, tempfile(pattern = "cube", tmpdir = getwd(),fileext = ".tif"))
-    }else if(tolower(format) =="netcdf"){
-      write_ncdf(data, tempfile(pattern = "cube", tmpdir = getwd(),fileext = ".nc"))
-    }else{
-      stop("The format entered is not supported")
-    }
+save_result <- function(data, format = "TIFF") {
+  if (is.null(format) || tolower(format) == "tiff" || format == "") {
+    write_tif(data,
+              tempfile(
+                pattern = "cube",
+                tmpdir = getwd(),
+                fileext = ".tif"
+              ))
+  } else if (tolower(format) == "netcdf") {
+    write_ncdf(data,
+               tempfile(
+                 pattern = "cube",
+                 tmpdir = getwd(),
+                 fileext = ".nc"
+               ))
+  } else{
+    stop("The format entered is not supported")
+  }
 
   # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="Processed data saved successfully")
+  msg <-
+    list(status = "SUCCESS",
+         code = "200",
+         message = "Processed data saved successfully")
 }
 
 
@@ -338,5 +416,8 @@ delete_files <- function() {
   # delete a directory
   unlink("./data", recursive = TRUE)
   # Response msg to user
-  msg <- list(status = "SUCCESS", code = "200",message ="All the processed files deleted")
+  msg <-
+    list(status = "SUCCESS",
+         code = "200",
+         message = "All the processed files deleted")
 }
